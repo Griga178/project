d09e96f5f61495b3b99858adfe7a6cacbf3b19a7
Griga178 <Grisha.tis@gmail.com>
2023-04-09 21:32:31 +0300

    12

--- start.py
+++ start.py
@@ -17,3 +17,33 @@ class Project():
         session.commit()
         self.table_object = table_object
         Project.exist_project.append(self)
+
+class User():
+    def __init__(self, name):
+        self.name = name
+
+class Subject():
+    def __init__(self, name):
+        self.name = name
+
+class Tasks():
+    def __init__(self):
+        self.tasks = []
+    def chose_maker(self, users):
+        # show_users()
+        # chose_user()
+    def chose_task(self):
+        pass
+    def create_last_date(self):
+        pass
+    def edit_task(self):
+        pass
+
+class ToBuy():
+    def __init__(self, user):
+        self.subjects = []
+    def chose_subject(self):
+        # show_subjects()
+        # create_subject()
+
+        self.subjects.append(subject)
